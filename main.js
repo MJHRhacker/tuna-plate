f4b69e215b7c91b2b5aebc548b92129134a58443
MJHR <majohr89@gmail.com>
2018-10-05 14:46:29 +0200

    slider html and js

--- main.js
+++ main.js
@@ -1,7 +1,23 @@
 'use strict';
 
-function main () {
-  console.log('JS Running');
+const slides = document.querySelectorAll('#slides .slide');
+const skip = document.querySelector('.skip-link');
+const slider = document.querySelector('.slider');
+let currentSlide = 0;
+const slideInterval = setInterval(nextSlide, 3000);
+
+function nextSlide () {
+  slides[currentSlide].className = 'slide';
+  currentSlide = (currentSlide + 1) % slides.length;
+  slides[currentSlide].className = 'slide showing';
+}
+
+skip.addEventListener('click', destroyScreen);
+
+function destroyScreen (e) {
+  if (e) {
+    slider.remove();
+  }
 }
 
 window.addEventListener('load', main);
